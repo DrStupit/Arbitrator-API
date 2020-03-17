5526ddbd8dd589362da70c4424ecd8c12899c620
Kerev Reddy <kerevreddy@hotmail.com>
2020-03-17 17:55:53 +0200

    [add] Physical Address class

--- couchbase-rest-api/Models/Council.cs
+++ couchbase-rest-api/Models/Council.cs
@@ -10,8 +10,18 @@ namespace couchbase_rest_api.Models
         public Guid? Id { get; set; }
         public Guid? UserId { get; set; }
         public string Name { get; set; }
-        public string PhysicalAddress { get; set; }
+        public Address PhysicalAddress { get; set; }
         public DateTime DateCreated { get; set; }
         public string Type { get; set; }
+    }
+
+    public class Address
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public string AreaCode { get; set; }
+        public string Province { get; set; }
     }
 }
